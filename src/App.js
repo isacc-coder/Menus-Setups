dbb2d63acf7ba0d4318d38d40ef907630f302d26
isacc firew <isaccfirew12@gmail.com>
2021-06-29 03:39:06 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import React from 'react'
 import Navbar from './Navbar'
 import Hero from './Hero'
 import Sidebar from './Sidebar'
-import Submenu from './Submenu'
+//import Submenu from './Submenu'
 function App() {
   return (
     <>
      <Navbar/>
      <Hero/>
      <Sidebar/>
-     <Submenu/>
+     
     </>
   )
 }
--- src/Navbar.js
+++ src/Navbar.js
@@ -3,21 +3,21 @@ import logo from './images/logo.svg'
 import { FaBars } from 'react-icons/fa'
 import {useGlobalContext} from './context';
 const Navbar = () => {
-  const {openSidebar,openSubmenu,closeSubmenu} = useGlobalContext();
+  const {openSidebar} = useGlobalContext();
   const displaySubmenu=(e)=>{
      const page = e.target.textContent;
      const tempBtn = e.target.getBoundingClientRect()
      const center = (tempBtn.left + tempBtn.right) / 2
      const bottom = tempBtn.bottom - 3
-     openSubmenu(page,{center,bottom})
+     //openSubmenu(page,{center,bottom})
   }
-  const handleSubmenu = (e)=>{
-    if(!e.target.classList.contains('link-btn')){
-      closeSubmenu()
-    }
-  }
+  //  const handleSubmenu = (e)=>{
+  // //   if(!e.target.classList.contains('link-btn')){
+  // //     closeSubmenu()
+  // //   }
+  //  }
   return(
-    <nav className="nav" onMouseOver={handleSubmenu}>
+    <nav className="nav" >
     <div className="nav-center">
        <div className="nav-header">
 <img src={logo} className="nav-logo" alt="stripe"/>
